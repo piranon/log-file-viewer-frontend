2e45a967035e7dc91cab1e75b5c1ba5e34ae2da7
piranon <piranon.n@gmail.com>
2017-05-11 20:40:22 +0700

    Create webpack config for production

---
+++ webpack.prod.config.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const webpack = require('webpack')
+const del = require('del')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].[contenthash].css"
+});
+
+class CleanPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply() {
+    del.sync(this.options.files);
+  }
+}
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new CleanPlugin({
+      files: ['dist/*']
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    extractSass
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: ['env', 'es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [ 'css-loader' ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }],
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  }
+}
